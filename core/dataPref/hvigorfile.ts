f67d7b3d13e7c486aa54618de42db39dcec4e256
jiewang41 <jiewang41@flemply.com>
2025-12-16 22:16:39 +0800

    feat: 新增隐私协议相关逻辑

---
+++ core/dataPref/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { HspTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
+
+export default {
+  system: HspTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+}
